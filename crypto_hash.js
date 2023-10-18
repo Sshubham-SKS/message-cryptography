186b59829402145640a1bc212803c8fbf720349b
Sshubham-SKS <121677326+Sshubham-SKS@users.noreply.github.com>
2023-10-18 19:41:53 +0530

    Update crypto_hash.js

--- crypto_hash.js
+++ crypto_hash.js
@@ -6,14 +6,14 @@ function hash(input) {
     return createHash('sha256').update(input).digest('base64');
 }
 
-let password = '@Bam-Shu';
+let password = '@Booki-Bee';
 // console.log(`The hash of ${password} is: `,hash(password));
 const hash1 = hash(password);
 console.log(hash1)
 
 //match two hash value
 
-password = '@Bam-Shu';
+password = '@Booki-Bee';
 const hash2 = hash(password);
 const match = hash1 === hash2;
 console.log(`does PASSWORD matches :-`, match ? 'Yes' : 'No')
